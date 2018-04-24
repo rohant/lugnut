67cfa399991663036a6de36255a6150900f48bbf
Roman Masyahin <r.masyahin@gmail.com>
2018-04-24 19:23:29 +0300

    Refactoring of the "Fake routes"  functionality

--- www/js/controllers/controller.map.js
+++ www/js/controllers/controller.map.js
@@ -1,204 +1,201 @@
 angular.module('app.controllers')
 
-.controller('MapCtrl', function ($scope, $rootScope, $log, $http, $state, $ionicLoading, Marker, Route) {
+.controller('MapCtrl', function ($scope, $rootScope, $log, $http, $state, $ionicLoading, Marker, Route, Geolocation, Config) {
 
-//    var watch, marker;
-//    var waypoints = [];
-//    var tracewaypoints;
-//
-//    $scope.isWatching = false;
-//    $scope.showActions = false;
-//
-//    // todo:
-//    $gps.simulationEnabled(true);
-//    
-//    $scope.$watch('debug.enabled', function(debug){
-//        $gps.simulationEnabled(debug && $scope.debug.simulation);
-//        $log.debug("debug.enabled:" + debug);
-//    });
-//
-//    $scope.$watch('debug.simulation', function(_n,_o){
-//        $gps.simulationEnabled(_n && $scope.debug.enabled);
-//        $log.debug("debug.simulation:" + $gps.simulation);
-//        if (_n != _o) $scope.init();
-//    });
-//
-//    //$scope.$watch('debug.showPoints', function(showPoins){
-//    //    $log.debug("debug.showPoins:" + showPoins);
-//    //});
-//
-//    /**
-//     *
-//     * @param {type} map
-//     * @return {undefined}
-//     */
-//    $scope.init = function () {
-//        $scope.map.setZoom(15);
-//        $scope.showReloadBtn = false;
-//
-//        $scope.loading = $ionicLoading.show({
-//            content: 'Getting current location...',
-//            showBackdrop: false
-//        });
-//
-//        if (!tracewaypoints) {
-//            tracewaypoints = new google.maps.Polyline({
-//                map: $scope.map,
-//                path: waypoints,
-//                strokeColor: "blue",
-//                strokeOpacity: 1.0,
-//                strokeWeight: 2
-//            });
-//        }
-//
-//        $gps.getCurrentPosition({
-//            enableHighAccuracy: true,
-//            maximumAge: 6000,
-//            timeout: 30000,
-//        }).then(function (position) {
-//            $log.debug('Got position:', position);
-//            $scope.showActions = true;
-//
-//            try {
-//                $scope.loading.hide();
-//            } catch (e) {
-//                $ionicLoading.hide();
-//            }
-//
-//            var myLatlng = new google.maps.LatLng(
-//                position.coords.latitude,
-//                position.coords.longitude
-//            );
-//
-//            $scope.map.setCenter(myLatlng);
-//            marker = Marker.current(position);
-//
-//        }, function (error) {
-//
-//            $log.error('Unable to get location: ' + error.message, error);
-//            $scope.showReloadBtn = true;
-//
-//            try {
-//                $scope.loading.hide();
-//            } catch (e) {
-//                $ionicLoading.hide();
-//            }
-//        });
-//    }
-//
-//    /**
-//     *
-//     * @param {type} map
-//     * @return {undefined}
-//     */
-//    $scope.mapCreated = function (map) {
-//        $scope.map = map;
-//        $scope.init(map);
-//        Marker.init(map);
-//    };
-//
-//    /**
-//     *
-//     * @return {undefined}
-//     */
-//    $scope.watchPosition = function () {
-//
-//        $log.debug("Start route recording..");
-//        $log.debug("Search GPS coordinates..");
-//
-//        if (!$scope.map) {
-//            return;
-//        }
-//
-//        waypoints = [];
-//        tracewaypoints.setPath(waypoints);
-//
-//        $rootScope.route = Route.createEmpty();
-//        $scope.isWatching = true;
-//
-//        if ($scope.debug.enabled && $scope.debug.simulation) {
-//            $log.info("Start simulation..");
-//        }
-//
-//        watch = $gps.watchPosition({
-//            enableHighAccuracy: true,
-//            maximumAge: 3600000,
-//            timeout: 30000,
-//        }).then(null, onError, onSuccess);
-//
-//        function onSuccess (position) {
-//            $log.debug('Got position:', position);
-//
-//            var myLatlng = new google.maps.LatLng(
-//                position.coords.latitude,
-//                position.coords.longitude
-//            );
-//
-//    //            if (!prevPosition || (
-//    //                Math.abs(prevPosition.coords.latitude - position.coords.latitude) > 0.000001
-//    //            ) || (
-//    //                Math.abs(prevPosition.coords.longitude - position.coords.longitude) > 0.000001
-//    //            ))
-//    //            {
-//    //                prevPosition = new google.maps.LatLng(
-//    //                    position.coords.latitude,
-//    //                    position.coords.longitude
-//    //                );
-//
-//    //                $scope.points.push({
-//    //                    latitude: prevPosition.coords.latitude,
-//    //                    longitude: prevPosition.coords.longitude
-//    //                });
-//    //            }
-//
-//            $scope.route.addPoint({
-//                lat: position.coords.latitude,
-//                lng: position.coords.longitude
-//            });
-//
-//            waypoints.push(myLatlng)
-//            tracewaypoints.setPath(waypoints);
-//
-//            $scope.map.setCenter(myLatlng);
-//            Marker.current(position);
-//        }
-//
-//        function onError (error) {
-//            $log.error('Unable to get location: ' + error.message, error);
-//        }
-//    };
-//
-//    /**
-//     *
-//     * @return {undefined}
-//     */
-//    $scope.stopWatchingPosition = function(){
-//        $scope.isWatching = false;
-//
-//        $log.debug('Stop route recording.');
-//        $log.debug('Saving route..');
-//
-//        $gps.clearWatch(watch);
-//
-//        tracewaypoints.setPath([]);
-//        //tracewaypoints.setMap(null);
-//
-//        //$scope.route.title = 'Route ' + new Date().getTime();
-//        //var routeId = $scope.route.save();
-//
-//        //if (routeId !== -1) {
-//        //    $state.go('app.view', {id: routeId})
-//        //}
-//        
-//        if ($scope.route.points.length > 3)
-//            $state.go('app.create')
-//    };
-//
-//    $scope.$on("$destroy", function () {
-//        console.log('$destroy')
-//        if (watch) {
-//            //watch.clearWatch(watch);
-//            $gps.clearWatch(watch);
-//        }
-//    });
+    var watch, marker;
+    var waypoints = [];
+    var tracewaypoints;
+
+    // todo:
+    Geolocation.simulationEnabled(true);
+    
+    $scope.isWatching = false;
+    $scope.showActions = false;
+    
+    /**
+     *
+     * @param {type} map
+     * @return {undefined}
+     */
+    $scope.init = function () {
+        $scope.map.setZoom(15);
+        $scope.showReloadBtn = false;
+
+        $scope.loading = $ionicLoading.show({
+            content: 'Getting current location...',
+            showBackdrop: false
+        });
+
+        if (!tracewaypoints) {
+            tracewaypoints = new google.maps.Polyline({
+                map: $scope.map,
+                path: waypoints,
+                strokeColor: "blue",
+                strokeOpacity: 1.0,
+                strokeWeight: 2
+            });
+        }
+
+        Geolocation.getCurrentPosition({
+            enableHighAccuracy: true,
+            maximumAge: 6000,
+            timeout: 30000,
+        }).then(function (position) {
+            $log.debug('Got position:', position);
+            $scope.showActions = true;
+
+            try {
+                $scope.loading.hide();
+            } catch (e) {
+                $ionicLoading.hide();
+            }
+
+            var myLatlng = new google.maps.LatLng(
+                position.coords.latitude,
+                position.coords.longitude
+            );
+
+            $scope.map.setCenter(myLatlng);
+            marker = Marker.current(position);
+
+        }, function (error) {
+
+            $log.error('Unable to get location: ' + error.message, error);
+            $scope.showReloadBtn = true;
+
+            try {
+                $scope.loading.hide();
+            } catch (e) {
+                $ionicLoading.hide();
+            }
+        });
+    }
+
+    /**
+     *
+     * @param {type} map
+     * @return {undefined}
+     */
+    $scope.mapCreated = function (map) {
+        $scope.map = map;
+        $scope.init(map);
+        Marker.init(map);
+    };
+
+    /**
+     *
+     * @return {undefined}
+     */
+    $scope.watchPosition = function () {
+
+        $log.debug("Start route recording..");
+        $log.debug("Search GPS coordinates..");
+
+        if (!$scope.map) {
+            return;
+        }
+
+        waypoints = [];
+        tracewaypoints.setPath(waypoints);
+
+        $rootScope.route = Route.createEmpty();
+        $scope.isWatching = true;
+
+        if (Config.debug.enabled && Config.debug.simulation) {
+            $log.info("Start simulation..");
+        }
+
+        watch = Geolocation.watchPosition({
+            enableHighAccuracy: true,
+            maximumAge: 3600000,
+            timeout: 30000,
+        }).then(null, onError, onSuccess);
+
+        function onSuccess (position) {
+            $log.debug('Got position:', position);
+
+            var myLatlng = new google.maps.LatLng(
+                position.coords.latitude,
+                position.coords.longitude
+            );
+
+    //            if (!prevPosition || (
+    //                Math.abs(prevPosition.coords.latitude - position.coords.latitude) > 0.000001
+    //            ) || (
+    //                Math.abs(prevPosition.coords.longitude - position.coords.longitude) > 0.000001
+    //            ))
+    //            {
+    //                prevPosition = new google.maps.LatLng(
+    //                    position.coords.latitude,
+    //                    position.coords.longitude
+    //                );
+
+    //                $scope.points.push({
+    //                    latitude: prevPosition.coords.latitude,
+    //                    longitude: prevPosition.coords.longitude
+    //                });
+    //            }
+
+            $scope.route.addPoint({
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+            });
+
+            waypoints.push(myLatlng)
+            tracewaypoints.setPath(waypoints);
+
+            $scope.map.setCenter(myLatlng);
+            Marker.current(position);
+        }
+
+        function onError (error) {
+            $log.error('Unable to get location: ' + error.message, error);
+        }
+    };
+
+    /**
+     *
+     * @return {undefined}
+     */
+    $scope.stopWatchingPosition = function(){
+        $scope.isWatching = false;
+
+        $log.debug('Stop route recording.');
+        $log.debug('Saving route..');
+
+        Geolocation.clearWatch(watch);
+
+        tracewaypoints.setPath([]);
+        //tracewaypoints.setMap(null);
+
+        //$scope.route.title = 'Route ' + new Date().getTime();
+        //var routeId = $scope.route.save();
+
+        //if (routeId !== -1) {
+        //    $state.go('app.view', {id: routeId})
+        //}
+        
+        if ($scope.route.points.length > 3)
+            $state.go('app.create')
+    };
+
+    $scope.$on("$destroy", function () {
+        console.log('$destroy')
+        if (watch) {
+            //watch.clearWatch(watch);
+            Geolocation.clearWatch(watch);
+        }
+    });
+    
+    $scope.$on("$ionicView.enter", function (event) {
+        
+        if ($scope.debug.simulation) {
+            
+            var fakeRoute = Geolocation
+                .getGeolocationSimulator()
+                .getFakeRoute();
+        
+            $log.info('Used the fake route: ' + fakeRoute.name);
+        }
+    });
 })
--- www/js/services/service.gps.js
+++ www/js/services/service.gps.js
@@ -2,16 +2,42 @@ angular.module('app.services')
 
 .provider('FakeRoutes', function() {
     
-    var routes = [];
+    var routes = [], current = null;
+    
+    /**
+     * 
+     * @param {Object} data
+     * @return {FakeRoute}
+     */
+    function FakeRoute (data) {
+        this.active = false;
+        this.id = routes.length;
+        angular.extend(this, data);
+    }
+    /**
+     * 
+     * @return {FakeRoute}
+     */
+    FakeRoute.prototype.apply = function () {
+        
+        for (var i in routes) 
+            routes[i].active = false;
+        
+        this.active = true;
+        current = this;
+        
+        return this;
+    }
     
     return {
         
         addRoute: function (route) {
-            routes.push(route);
+            routes.push(new FakeRoute(route));
             return this;
         },
         
         $get: function(){
+            
             return {
                 getRoute: function(id){
                     return routes[id]
@@ -19,133 +45,65 @@ angular.module('app.services')
                 getRoutes: function(){
                     return routes;
                 },
+                getActiveRoute: function(){
+
+                    if (!current) 
+                    {
+                        for (var i in routes) {
+                            if (routes[i].active) {
+                                current = routes[i];
+                                break;
+                            }
+                        }
+
+                        if (!current) {
+                            current = routes[0];
+                        }
+                    }
+                    
+                    return current;
+                },
             };
         }
     };
 })
 
-.config(function(FakeRoutesProvider) {
-    
-    FakeRoutesProvider
-    .addRoute({
-        default: true,
-        name: 'Fake route #1',
-        speed: 60,
-        interval: 2000,
-        points: [
-            [37.803210, -122.285347],
-            [37.803738, -122.287744],
-            [37.808245, -122.286561],
-            [37.809588, -122.286105],
-            [37.809875, -122.287653],
-            [37.810235, -122.289261],
-            [37.810427, -122.291173],
-            [37.811697, -122.290566],
-            [37.813280, -122.290232],
-            [37.816013, -122.289201],
-            [37.816708, -122.287380],
-            [37.815485, -122.284892],
-            [37.815126, -122.283223],
-            [37.814454, -122.280643],
-            [37.814119, -122.278367]
-        ]
-    })
-    .addRoute({
-        name: 'Fake route #2',
-        speed: 100,
-        interval: 1000,
-        points: [
-            [37.805033, -122.293816],
-            [37.805375, -122.294957],
-            [37.805826, -122.297063],
-            [37.806137, -122.298105],
-            [37.807085, -122.297358],
-            [37.808158, -122.296492],
-            [37.809184, -122.295665],
-            [37.810334, -122.294819],
-            [37.811133, -122.292882],
-            [37.810881, -122.291729],
-            [37.810294, -122.289287],
-            [37.811169, -122.288892],
-            [37.811792, -122.288786],
-            [37.811528, -122.287754],
-            [37.811157, -122.286146]
-        ]
-    })
-    .addRoute({
-        name: 'Fake route #3',
-        speed: 130,
-        interval: 500,
-        points: [
-            [37.800735, -122.280453],
-            [37.801057, -122.281212],
-            [37.801487, -122.282307],
-            [37.801912, -122.283260],
-            [37.802308, -122.284205],
-            [37.802833, -122.286292],
-            [37.803049, -122.288690],
-            [37.802909, -122.290948],
-            [37.802341, -122.294311],
-            [37.802142, -122.295996],
-            [37.802367, -122.298603],
-            [37.802998, -122.300497],
-            [37.803702, -122.301704],
-            [37.805143, -122.303324],
-            [37.807152, -122.304397],
-            [37.809266, -122.304560],
-            [37.811945, -122.303407],
-            [37.813312, -122.301882],
-            [37.814493, -122.300388],
-            [37.815841, -122.298461],
-            [37.816692, -122.297300],
-            [37.818334, -122.296253]
-        ]
-    });
-})
-
-.factory('GPSSimulator', function ($q, $log, $timeout, FakeRoutes) {
-    
-    console.log('FakeRoutes', FakeRoutes)
-    
-    
-    return {}
-    
-    
+.factory('GeolocationSimulator', function ($q, $log, $timeout, FakeRoutes) {
     
     var simulator = {
-
-        fakeID: 0,
-        fakeRoutes: [],
+        
+        fakeRoute: null,
 
         /**
          * 
-         * @return {Array}
+         * @param {Object} route
+         * @return {simulator}
          */
-        getFakeRoutes: function(){
+        setFakeRoute: function(route){
+            this.fakeRoute = route.apply();
             
-//            for (var i in this.fakeRoutes) {
-//                if (this.fakeRoutes[i].hasOwnProperty('default')) {
-//                    this.fakeID = i;
-//                }
-//            }
+            $log.info('Fake route: "' + route.name + '"');
             
-            return this.fakeRoutes;
+            return this;
         },
-
+        
         /**
          * 
-         * @param {Integer} id
          * @return {Object}
          */
-        getFakeRoute: function(id){
-            return FakeRoutes.getRoute(id);
+        getFakeRoute: function(){
+            
+            if (!this.fakeRoute)
+                throw new Error("Fake route is wrong!");
+            
+            return this.fakeRoute;
         },
 
         /**
          *
          * @return {unresolved}
          */
-        getCurrentPosition: function () {//debugger;
+        getCurrentPosition: function () {
 
             var route = this.getFakeRoute(this.fakeID);
 
@@ -210,16 +168,19 @@ angular.module('app.services')
         },
     };
     
+    simulator.setFakeRoute(FakeRoutes.getActiveRoute());
+    
     return simulator;
 })
 
-.factory('GPSEngineSwither', function ($q, $log, $cordovaGeolocation, GPSSimulator) {
+.factory('Geolocation', function ($q, $log, $cordovaGeolocation, GeolocationSimulator) {
     
     var gps = {
 
         simulation: false,
-        simulator: GPSSimulator,
-
+        simulator: null,
+        geolocation: null,
+        
         /**
          * 
          * @param {Boolean} enable
@@ -229,44 +190,76 @@ angular.module('app.services')
             this.simulation = enable;
             return this;
         },
-
         /**
          * 
-         * @return {simulator|$cordovaGeolocation}
+         * @param {$cordovaGeolocation} geolocation
+         * @return {gps}
+         */
+        setGeolocation: function (geolocation) {
+            this.geolocation = geolocation;
+            return this;
+        },
+        /**
+         * 
+         * @return {$cordovaGeolocation}
+         */
+        getGeolocation: function () {
+            return this.geolocation;
+        },
+        /**
+         * 
+         * @param {GeolocationSimulator} simulator
+         * @return {gps}
+         */
+        setGeolocationSimulator: function (simulator) {
+            this.simulator = simulator;
+            return this;
+        },
+        /**
+         * 
+         * @return {GeolocationSimulator}
+         */
+        getGeolocationSimulator: function () {
+            return this.simulator;
+        },
+        /**
+         * 
+         * @return {Object}
          */
         getSource: function(){
             var deferred = $q.defer();
 
             ionic.Platform.ready(function() {
-                var source = this.simulation ? this.simulator : $cordovaGeolocation;
+                
+                var source = gps.simulation 
+                    ? gps.getGeolocationSimulator() 
+                    : gps.getGeolocation();
+                
                 deferred.resolve(source);
             });
 
-            //return this.simulation ? this.simulator : $cordovaGeolocation;
-
             return deferred.promise;
         },
-
         /**
          *
          * @return {unresolved}
          */
-        getCurrentPosition: function () {//debugger;
+        getCurrentPosition: function () {
+            var arguments = arguments;
             return this.getSource().then(function(source){
                 return source.getCurrentPosition.apply(source, arguments);
             });
         },
-
         /**
          *
          * @return {unresolved}
          */
         watchPosition: function () {
+            var arguments = arguments;
             return this.getSource().then(function(source){
-                return (this.watcher = source.watchPosition.apply(source, arguments));
+                return (gps.watcher = source.watchPosition.apply(source, arguments));
             });
         },
-
         /**
          *
          * @return {unresolved}
@@ -277,6 +270,9 @@ angular.module('app.services')
             }
         },
     };
+    
+    gps.setGeolocation($cordovaGeolocation);
+    gps.setGeolocationSimulator(GeolocationSimulator);
 
     return gps;
 })
