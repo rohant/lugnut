45445845d4fe6eeb929fa16fb14da4be50c32f13
Roman Masyahin <r.masyahin@gmail.com>
2018-04-25 15:45:40 +0300

    start implementation the advanced search functionality

---
+++ www/js/controllers/controller.route-search-advanced.js
@@ -0,0 +1,28 @@
+angular.module('app.controllers')
+
+.controller('RouteAdvancedSearchCtrl', function ($scope, $state, Route, AuthService) {
+    
+    if (!AuthService.isLoggedIn()) {
+        
+        // set "to back" function
+        AuthService.toBack = function(){
+            AuthService.toBack = null;
+            $state.go('app.search-advanced');
+        }
+        
+        $state.go('app.signin');
+    }
+    
+    $scope.criteria = {
+        query: '',
+    }
+    
+    $scope.reload = function(){
+        $scope.processing = true;
+        
+        return Route.findAll($scope.criteria).then(function(items){
+            $scope.processing = false;
+            $scope.routes = items;
+        });
+    }
+});
