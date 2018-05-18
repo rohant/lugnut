412317439795a6f4f546a47bbc2f7cef8efe7959
Roman Masyahin <r.masyahin@gmail.com>
2018-05-18 17:12:14 +0300

    implementation the ratings functionality

--- www/js/controllers/controller.route-detail.js
+++ www/js/controllers/controller.route-detail.js
@@ -1,9 +1,16 @@
 angular.module('app.controllers')
 
-.controller('RouteDetailCtrl', function ($scope, $state, $log, Route, GoogleStaticMap) {
+.controller('RouteDetailCtrl', function ($scope, $state, $log, Route, ApiService) {
     $scope.model = null;
     
     Route.findOne($state.params.id).then(function (model) {
         $scope.model = model.with('user');
+        
+        ApiService.post('statistic/track-event', {
+            entity: 'app\\models\\Route',
+            entityID: model.id,
+            eventType: 'route-view',
+            eventData: {},
+        });
     });
 });
